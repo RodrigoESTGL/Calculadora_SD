16902bb20ddae6a7dc466e1ceb97418ecc42bade
Ricardo Cunha <cunhatrash@gmail.com>
2024-12-28 03:28:27 +0000

    Bigserial bug

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Operacoes (
-    id BIGINT PRIMARY KEY,  -- Chave primária do tipo BIGINT
+    id BIGSERIAL PRIMARY KEY,  -- Chave primária do tipo BIGINT
     op VARCHAR(100) NOT NULL,  -- Coluna 'op' do tipo VARCHAR com máximo de 100 caracteres, não nula
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP  -- Coluna 'timestamp' com valor padrão sendo a hora atual
 );
